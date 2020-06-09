e5ac53ab25258901af4e95763d869a2c98aae597
Harshit <hshrivastava1996@gmail.com>
2020-06-10 01:44:59 +0530

    Solve negative index exercise

---
+++ js-exercises/negative-index/negativeIndex.js
@@ -0,0 +1,27 @@
+const getPositiveIndex = (prop, length) => {
+    let Index = Number(prop);
+    let PositiveIndex = Index < 0 ? length + Index : Index;
+    return PositiveIndex;
+}
+
+const Handler = {
+    get: (target, prop, receiver) => {
+        if (isNaN(Number(prop))) {
+            return Reflect.get(target, prop, receiver);
+        }
+        let PositiveIndex = getPositiveIndex(prop, target.length);
+        return target[PositiveIndex];
+    },
+    set: (target, prop, val, receiver) => {
+        let PositiveIndex = getPositiveIndex(prop, target.length);
+        return Reflect.set(target, PositiveIndex, val, receiver);
+    }
+}
+
+
+function negativeIndex(arr) {
+    if (!(arr instanceof Array)) throw new TypeError("Only arrays are supported");
+    return new Proxy(arr, Handler);
+}
+
+export { negativeIndex };
